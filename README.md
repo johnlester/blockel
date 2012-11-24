58becea7a50278a63b61ecffe8be43870c050c64
John Lester <johnlester@gmail.com>
2012-11-24 09:31:14 -0500

    coming along

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 blockel
 =======
+
+Progressively subdivides an image into blocks of color, based on a reference image (like a photo)
--- blockel.go
+++ blockel.go
@@ -7,10 +7,12 @@ package blockel
 import (
 	"image"
 	"image/color"
-	"image/jpeg"
+	_ "image/jpeg"
 	// "fmt"
-	// "runtime"
-	//	"log"
+	"os"
+	"log"
+	// "github.com/ajstarks/svgo"
+
 )
 
 var (
@@ -18,41 +20,142 @@ var (
 )
 
 const (
+	BlockSetReservedSize = 40
 )
 
-
+type Dimensions struct {
+	// Full new picture Dimensions are 0.0, 0.0 to width, height of original image
+	left, right, top, bottom float64
+}
 
 //Block of color
 type Block struct {
-	top float64
-	bottom float64
-	right float64
-	left float64
-	area float64
-	color Color
-	totalColorDelta float64
-	averageColorDelta float64
-	//Score		// Value to sort on; customizable score to pick which block to divide
+	Dim Dimensions
+	BlockColor color.Color
+	Score float64 		// Value to sort on; customizable score to pick which block to divide
 }
 
-//Color interface--to be implmented by RBG, LAB, etc.
-type Color interface {
-
+func NewBlock(d Dimensions, c color.Color) Block {
+	newBlock := Block{Dim: d, BlockColor: c}
+	return newBlock
 }
+
+type BlockSet struct {
+	Blocks []Block
+	ReferenceImage *image.Image	
+}
+
+func NewBlockSet(refImageFilePath string) *BlockSet {
+	// Open the reference image file
+	file, err := os.Open(refImageFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	
+	// Decode the image
+	m, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	//Create first Block
+	bounds := m.Bounds()
+	imgLeft := bounds.Min.X
+	imgRight := bounds.Max.X
+	imgTop := bounds.Min.Y
+	imgBottom := bounds.Max.Y
+	imgWidth := imgRight - imgLeft
+	imgHeight := imgBottom - imgTop
+	dim := Dimensions{left: 0.0, right: float64(imgWidth), top: 0.0,  bottom: float64(imgHeight)}
+	aveColor := averageRGB(m, dim)
+	firstBlock := NewBlock(dim, aveColor)
+
+	
+	
+	//Create a BlockSet
+	blocksInSet := make([]block, 1, BlockSetReservedSize)
+	newBlockSet = BlockSet{blocks: blocksinSet, referenceImage: &m}
+	
+	return newBlockSet
+}
+
+func (bs *BlockSet) AddBlock(b *Block) {
+	//Calculate score for Block
+	b.Score = CalculateScore(bs.ReferenceImage, *b)
+}
+
+func CalculateScore(m *image.Image, b Block) float64 {
+	rect := dimensionsOnImage(m, b.Dim)
+	minx := rect.Min.X
+	maxx := rect.Max.X
+	miny := rect.Min.Y
+	maxy := rect.Max.Y
+	
+	var delta uint64 = 0
+	for y := miny; y < maxy; y++ {
+		for x := minx; x < maxx; x++ {
+			r, g, b, a := m.At(x, y).RGBA()
+			//sum of squares
+			delta += 0
+		}
+	}
+	return float64(delta)
+}
+
+func dimensionsOnImage(m *image.Image, dimensions Dimensions) image.Rect {
+	bounds := m.Bounds()
+	left := bounds.Min.X
+	top := bounds.Min.Y
+	
+	newLeft := left + int(dimensions.left)
+	newRight := left + int(dimensions.right)
+	newTop := top + int(dimensions.top)
+	newBottom := top + int(dimensions.bottom)
+	
+	return image.Rect(newLeft, newTop, newRight, newBottom)
+}
+
+func averageRGB(m image.Image, dimensions) color.Color {
+	//Calculate average color
+
+
+	var rt, gt, bt, at uint64
+	for y := miny; y < maxy; y++ {
+		for x := minx; x < maxx; x++ {
+			r, g, b, a := m.At(x, y).RGBA()
+			rt += uint64(r)
+			gt += uint64(g)
+			bt += uint64(b)
+			at += uint64(a)
+		}
+	}
+	pixelCount := (uint64(maxy - miny) * uint64(maxx - minx))	
+	ra := uint8(rt / (256 * pixelCount))
+	ga := uint8(gt / (256 * pixelCount))
+	ba := uint8(bt / (256 * pixelCount))
+	aa := uint8(at / (256 * pixelCount))
+	aveColor := color.RGBA{ra, ga, ba, aa}
+	return aveColor
+}
+
+
+//Criteria: takes 2 or more block-sets covering the same area and rank orders them
+// type Criteria interface {
+// 	Score(BlockSet)
+// }
+
+
+
 
 //Represents a way of dividing up a single block into 2 or more blocks
-type Split struct {
-	xSplits []float64	//should be 0 < x < 1
-	xSplits []float64	//should be 0 < x < 1
-}
+// type Split struct {
+// 	xSplits []float64	//should be 0 < x < 1
+// 	xSplits []float64	//should be 0 < x < 1
+// }
 
 
 //Collection of blocks
-//Can be sorted by color deltas or other characteristics
-var Blocks = make([]Blocks, 1, 100)
-
-
-
 
 //Function that returns slice of possible Splits to try
 //
